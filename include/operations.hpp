14682bec621cb638210d1521d174328154e0b66d
Iury Gama <Iuryempregos@gmail.com>
2024-07-18 19:58:21 -0300

    refactor code

--- include/operations.hpp
+++ include/operations.hpp
@@ -14,15 +14,13 @@ namespace ees
         Operations();
         auto get_meter_list() -> std::vector<EnergyMeter>;
 
-        auto filter_by_line(const Lines &line) -> std::vector<EnergyMeter>;
         auto filter_by_line(const std::string &line) -> std::vector<EnergyMeter>;
         
         auto get_all_lines() -> std::vector<Lines>;
         auto find_meter_by_id(const int &id) -> EnergyMeter;
-        auto check_if_meter_already_exists(const EnergyMeter &meter) -> bool;
 
         // CRUD
-        auto add_new_model(const EnergyMeter & new_meter) -> bool; // CREATE
-        auto remove_model(const int &id) -> bool; // DELETE
+        void add_new_model(const EnergyMeter & new_meter);
+        auto remove_model(const int &id) -> bool;
     };
 } // namespace ees
--- source/operations.cpp
+++ source/operations.cpp
@@ -5,6 +5,7 @@
 #include "ares.hpp"
 #include "utils.hpp"
 #include "project_exceptions.hpp"
+#include "validations.hpp"
 
 #include <iostream>
 #include <algorithm>
@@ -12,27 +13,29 @@
 
 namespace ees
 {
+    Validations validations;
+
     Operations::Operations()
     {
         meter_list =
             {
-                Ares(1,"7021"),
-                Ares(2,"7023"),
-                Ares(3,"7031"),
-                Ares(4,"8023"),
-                Ares(5,"8023 15"),
-                Ares(6,"8023 200"),
-                Cronos(7,"6001-A"),
-                Cronos(8,"6021-A"),
-                Cronos(9,"6021L"),
-                Cronos(10,"6003"),
-                Cronos(11,"7023"),
-                Cronos(12,"7023L"),
-                Cronos(13,"7023 2,5"),
-                Apolo(14,"6031"),
-                Zeus(15,"8021"),
-                Zeus(16,"8023"),
-                Zeus(17,"8031"),
+                Ares(1, "7021"),
+                Ares(2, "7023"),
+                Ares(3, "7031"),
+                Ares(4, "8023"),
+                Ares(5, "8023 15"),
+                Ares(6, "8023 200"),
+                Cronos(7, "6001-A"),
+                Cronos(8, "6021-A"),
+                Cronos(9, "6021L"),
+                Cronos(10, "6003"),
+                Cronos(11, "7023"),
+                Cronos(12, "7023L"),
+                Cronos(13, "7023 2,5"),
+                Apolo(14, "6031"),
+                Zeus(15, "8021"),
+                Zeus(16, "8023"),
+                Zeus(17, "8031"),
             };
     }
 
@@ -44,57 +47,36 @@ namespace ees
     auto Operations::find_meter_by_id(const int &id) -> EnergyMeter
     {
         auto it = std::find_if(meter_list.begin(), meter_list.end(),
-         [&id](EnergyMeter &meter)
-        { return meter.get_id() == id; });
+                               [&id](EnergyMeter &meter)
+                               { return meter.get_id() == id; });
 
         if (it != meter_list.end())
         {
             return *it;
         }
 
-        throw ees::NotFound{};
-    }
-
-    auto Operations::filter_by_line(const Lines &line) -> std::vector<EnergyMeter>
-    {
-        std::vector<EnergyMeter> filtered_meter_list;
-
-        std::copy_if(meter_list.begin(), meter_list.end(), std::back_inserter(filtered_meter_list),
-                     [&line](EnergyMeter meter)
-                     { return meter.get_line() == line; });
-        return filtered_meter_list;
+        throw expections::NotFound{};
     }
 
     auto Operations::filter_by_line(const std::string &line) -> std::vector<EnergyMeter>
     {
+        validations.check_if_line_exists(line);
+        
         std::vector<EnergyMeter> filtered_meter_list;
 
         std::copy_if(meter_list.begin(), meter_list.end(), std::back_inserter(filtered_meter_list),
                      [&line](EnergyMeter meter)
-                     { return convert_enumline_to_string(meter.get_line()) == line; });
+                     { return to_uppercase(convert_enumline_to_string(meter.get_line())) == to_uppercase(line); });
         return filtered_meter_list;
     }
 
-    auto Operations::check_if_meter_already_exists(const EnergyMeter &new_meter) -> bool
+    void Operations::add_new_model(const EnergyMeter &new_meter)
     {
-        for (const auto &meter : meter_list)
-        {
-            if (meter.get_id() == new_meter.get_id())
-            {   
-                return false; // meter already exists;
-            }
-        }
-        throw ees::ThisMeterAlreadyExist{};
-    }
+        // if is equal a true -> means there is already a meter.
+        validations.check_if_meter_already_exists(new_meter);
 
-    auto Operations::add_new_model(const EnergyMeter &new_meter) -> bool
-    {
-        if (check_if_meter_already_exists(new_meter))
-        {
-            meter_list.push_back(new_meter);
-            return true;
-        }
-        return false;
+        // if is not already exists. then, create.
+        meter_list.push_back(new_meter);
     }
 
     auto Operations::remove_model(const int &id) -> bool // DELETE
@@ -107,11 +89,12 @@ namespace ees
             meter_list.erase(it); // Remove  o medidor encontrado
             return true;
         }
-        throw ees::NotFound{};
+        throw expections::NotFound{};
     }
 
     auto Operations::get_all_lines() -> std::vector<Lines>
     {
         return {Lines::ARES, Lines::APOLO, Lines::CRONOS, Lines::ZEUS, Lines::UNKNOWN};
     }
+
 } // namespace ees
